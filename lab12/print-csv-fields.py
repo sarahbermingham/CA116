c9f71e4e8853efe1ebf5dda7b3afca1c7b77d580
sarahbermingham <45370107+sarahbermingham@users.noreply.github.com>
2018-11-27 21:58:41 +0000

    Update print-csv-fields.py

--- lab12/print-csv-fields.py
+++ lab12/print-csv-fields.py
@@ -4,23 +4,25 @@ s = raw_input()
 a = []
 
 while s != "end":
-	a.append(s)
-	s = raw_input()
+    a.append(s)
+    s = raw_input()
 
 n = input()
 
-start = 0
-i = 0
+j = 0
+while j < len(a):
+    start = 0
+    i = 0
+    while i < n:
+        while start < len(a[j]) and a[j][start] != ",":
+            start = start + 1
 
-while a[i] < n:
-	while start < len(s) and s[start] != ",":
-		start = start + 1
+        start = start + 1
+        i = i + 1
 
-	start = start + 1
-	i = i + 1
+    end = start + 1
+    while end < len(a[j]) and a[j][end] != ",":
+        end = end + 1
 
-end = start + 1
-while end < len(s) and s[end] != ",":
-	end = end + 1
-
-print s[start:end]
+    print a[j][start:end]
+    j += 1

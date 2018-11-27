5df1672b7be4db640c8f4ddc5fe749f0f72c6def
sarahbermingham <45370107+sarahbermingham@users.noreply.github.com>
2018-11-27 20:23:47 +0000

    Update de-dup-lines.py

--- lab09/de-dup-lines.py
+++ lab09/de-dup-lines.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 
 s = raw_input()
-a = []
-
-a.append(s)
+seen = []
+words = []
 
 while s != "end":
-    i = 0
-    dup = 0
-    while i < len(a):
-        if a[i] == s:
-            dup = dup + 1
-        i = i + 1
-    if 0 == dup:
-        a.append(s)
+    words.append(s)
     s = raw_input()
 
-print a
 i = 0
-while i < len(a):
-    print a[i]
-    i = i + 1
+while i < len(words):
+    j = 0
+    while j < len(seen) and seen[j] != words[i]:
+        j += 1
+
+    if len(seen) <= j:
+        print words[i]
+        seen.append(words[i])
+    i += 1
